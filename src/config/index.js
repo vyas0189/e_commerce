a96aa1cd97b4448839ad96ef5a355726cfe8db32
Vyas Ramankulangara <vyas01890@outlook.com>
2020-03-17 00:27:08 -0500

    config server test

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,8 @@
+import cors from 'cors';
+import express, { json } from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
 export const {
     APP_PORT = 4000,
     NODE_ENV = 'development',
@@ -9,3 +14,29 @@ export const {
 export const IN_PROD = NODE_ENV === 'production';
 
 export const MONGO_URI = IN_PROD ? process.env.MONGO_URI : `mongodb://${MONGO_HOST}/${MONGO_DATABASE}`;
+
+const app = express();
+app.use(cors());
+app.use(morgan('common'));
+app.use(helmet());
+app.use(json({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.json({ welcome: 'CougarCS Backend 🐯' });
+});
+
+app.use((req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+});
+app.use((error, req, res) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: error.message,
+        stack: IN_PROD ? '🥞' : error.stack,
+    });
+});
+
+export default app;
