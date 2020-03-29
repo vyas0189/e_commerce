87bed1d554952a3567caae5975c747d92e565cf0
Vyas Ramankulangara <vyas01890@outlook.com>
2020-03-29 02:02:43 -0500

    setup react

---
+++ client/src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Navbar from './Navbar';
+
+const App = () => {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          {/* <Route exact path="/products" component={Products} /> */}
+        </Switch>
+      </Fragment>
+    </Router>
+  );
+}
+
+export default App;
