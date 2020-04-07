42e76c97586059657dffa1e28281272014c44395
Vyas Ramankulangara <vyas01890@outlook.com>
2020-04-06 20:34:00 -0500

    added cors

--- src/config/index.js
+++ src/config/index.js
@@ -47,9 +47,8 @@ export const SESSION_OPTIONS = {
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
 };
 
-
 const app = express();
-app.use(cors());
+app.use(cors({ origin: IN_PROD ? 'https://e-commerce-software-eng.herokuapp.com/' : '*' }));
 app.use(morgan('common'));
 app.use(helmet());
 app.use(json({ extended: false }));
@@ -63,20 +62,6 @@ app.get('/api', (req, res) => {
 app.use('/api/product', product);
 app.use('/api/address', address);
 app.use('/api/user', user);
-
-// app.use((req, res, next) => {
-//     const error = new Error(`Not Found - ${req.originalUrl}`);
-//     res.status(404);
-//     next(error);
-// });
-// app.use((error, req, res) => {
-//     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-//     res.status(statusCode);
-//     res.json({
-//         message: error.message,
-//         stack: IN_PROD ? '🥞' : error.stack,
-//     });
-// });
 
 if (IN_PROD) {
     app.use(express.static(join(__dirname, '../../client/build')));
