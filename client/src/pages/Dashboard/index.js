009a66c7bb50858a835e44ebc4872addc517168d
Vyas Ramankulangara <vyas01890@outlook.com>
2020-04-22 17:32:19 -0500

    Added routes and components

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useStoreState } from 'easy-peasy';
+import React from 'react';
+import AdminDashboard from '../../components/Dashboard/Admin';
+import UserDashboard from '../../components/Dashboard/User';
 
 const Dashboard = () => {
+
+    const loading = useStoreState(state => state.user.loading);
+    const user = useStoreState(state => state.user.user);
+
     return (
         <div>
-            Dashboard
+            {loading || !user ? <h1>Loading...</h1> : (
+                !loading && user.role === 'admin' ? <AdminDashboard /> :
+                    !loading && user.role === 'user' ? <UserDashboard /> : null
+            )}
         </div>
     )
 }
