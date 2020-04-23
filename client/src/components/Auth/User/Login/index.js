91f27d52fdd0819f77ab303e2bbe00073427f7f1
Justin Nguyen <Vu@Justins-MBP.attlocal.net>
2020-04-23 15:20:38 -0500

    Initial design of register and login

--- client/src/components/Auth/User/Login/index.js
+++ client/src/components/Auth/User/Login/index.js
@@ -8,21 +8,18 @@ import "./Login.css";
 const Login = () => {
   return (
     <Container>
-      <Form class="center">
+      <Form>
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="email" placeholder="Enter username" />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Enter password" />
         </Form.Group>
         <Button variant="primary" type="submit">
-          Submit
+          Log In
         </Button>
       </Form>
     </Container>
--- client/src/components/Auth/User/Register/index.js
+++ client/src/components/Auth/User/Register/index.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from "react";
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Container from "react-bootstrap/Container"
+import "./Register.css";
 
 const Register = () => {
-    return (
-        <div>
-            User Register
-        </div>
-    )
-}
+  return (
+    <Container>
+      <Form>
+        <Form.Group controlId="formBasicFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="firstName" placeholder="First Name" />
+        </Form.Group>
 
-export default Register 
+        <Form.Group controlId="formBasicLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="lastName" placeholder="Last Name" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="username" placeholder="Username" />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Register
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default Register
