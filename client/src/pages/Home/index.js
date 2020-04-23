bf6db0fb14f86929792c7c4ecec2251a4e91990f
Vyas Ramankulangara <vyas01890@outlook.com>
2020-04-23 01:52:56 -0500

    styled home page

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            Home Page
-        </div>
+        <>
+            <section className="indexSection">
+                <div class="card-group">
+                    <div class="card-body" id="card1">
+                        <h1 class="card-title">Modern.</h1>
+                    </div>
+                </div>
+                <div class="card-group">
+                    <div class="card-body" id="card2">
+                        <h1 class="card-title">Style.</h1>
+                    </div>
+                </div>
+                <div class="card-group">
+                    <div class="card-body" id="card3">
+                        <h1 class="card-title">Modern.</h1>
+                    </div>
+                </div>
+            </section>
+
+        </>
     )
 }
 
