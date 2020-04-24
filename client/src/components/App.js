54f2393e7a1310661d07b0bce766da984faba8b4
kmngn <59036246+kmngn@users.noreply.github.com>
2020-04-24 01:54:12 -0500

    added cart page and modified products page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,6 +9,7 @@ import Products from '../pages/Products';
 import AdminLogin from './Auth/Admin';
 import Login from './Auth/User/Login';
 import Register from './Auth/User/Register';
+import Checkout from '../pages/Checkout';
 import NavbarComponent from './Navbar';
 import PrivateRoute from './PrivateRoutes';
 import PrivateRouteAdmin from './PrivateRoutes/Admin';
@@ -33,6 +34,7 @@ const App = () => {
           <Route exact path="/admin" component={AdminLogin} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/checkout" component={Checkout} />
           <PrivateRoute exact path="/cart" component={Cart} />
           <PrivateRouteAdmin exact path="/dashboard" component={Dashboard} />
           <Route component={() => <h1>Not Found!</h1>} />
--- client/src/pages/Cart/index.js
+++ client/src/pages/Cart/index.js
@@ -1,9 +1,100 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useStoreActions, useStoreState } from 'easy-peasy';
+import { Card, Button, Container, Row, Col, Form, ListGroupItem, ListGroup, Figure } from 'react-bootstrap';
 
 const Cart = () => {
+
+    const cart = useStoreState(state => state.products.cart);
+    const loading = useStoreState(state => state.products.loading)
+    const getCart = useStoreActions(actions => actions.products.getCart);
+
+    const styles = {
+        h1: {
+        fontSize: '15px',
+        color: 'black'
+        },
+        img:{
+            height: '150px',
+            width: '150px',
+            objectFit: 'cover'
+        }
+    };
+
+    useEffect(() => {
+        getCart()
+    }, [])
+
     return (
         <div>
-            Cart
+            {loading ? <h1>Loading... </h1>:
+        <Container>
+            <h1>Shopping Cart</h1><br></br>
+            <h1>{cart.length} items:</h1>
+            <Row>
+                <Col md='8'>
+                {cart.map((product, index)=> (
+                    <ListGroup>  
+                        <Figure>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col xs='4'>
+                                        <img src={product.image} style = {styles.img}/>
+                                    </Col>
+                                    <Col xs='8'>
+                                        <Figure.Caption float = 'right'>
+                                        {product.name}<br></br><br></br>
+                                        {product.description}<br></br><br></br>
+                                        <b>Price: ${product.price}</b><br></br>
+                                        <b>Quantity: 1</b>
+                                        </Figure.Caption>
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                        </Figure>
+                     </ListGroup>
+                    ))}
+                    </Col>
+            <Col md='4'>
+            <ListGroup>
+                    <ListGroupItem><h1 style={styles.h2}>Order Summary</h1></ListGroupItem>
+                    <ListGroupItem>
+                    {
+                        <Row>
+                            <Col xs='8'><p>(prod x quantity)</p></Col>
+                            <Col xs='4'><p>$</p></Col>
+                        </Row>
+                    }
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        <Row>
+                            <Col xs='8'><p>Subtotal</p></Col>
+                            <Col xs='4'><p>$</p></Col>
+                        </Row>
+                        <Row>
+                            <Col xs='8'><p>Shipping</p></Col>
+                            <Col xs='4'><p>$</p></Col>
+                        </Row>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        <Row>
+                            <Col xs='8'><p>Total</p></Col>
+                            <Col xs='4'><b style={{fontSize: '25px'}}>$</b></Col>
+                        </Row>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        <center>
+                        <a href='/checkout'><Button variant="primary" type="submit">
+                            Checkout
+                        </Button>
+                        </a>
+                        </center>
+                    </ListGroupItem>
+                 </ListGroup>S
+            </Col>
+            </Row>
+        </Container> 
+        }
         </div>
     )
 }
