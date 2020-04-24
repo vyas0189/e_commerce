6f32d3a4f066cf3d2a5c83686858fff6d71a4378
Vyas Ramankulangara <vyas01890@outlook.com>
2020-04-24 01:58:05 -0500

    refactor login

--- client/src/components/Auth/User/Login/index.js
+++ client/src/components/Auth/User/Login/index.js
@@ -1,55 +1,53 @@
-import { React, useState } from 'react';
-import { useStoreActions, useStoreState } from 'easy-peasy'
-
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-import Container from 'react-bootstrap/Container'
-import "./Login.css";
+import { useStoreActions, useStoreState } from 'easy-peasy';
+import { MDBBtn, MDBCard, MDBCol, MDBContainer, MDBInput, MDBRow } from 'mdbreact';
+import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
+import Home from '../../../../pages/Home';
+import './Login.css';
 
 const Login = () => {
-
-  const [formData, setFormData] = useState( {
+  const [formData, setFormData] = useState({
     username: '',
     password: ''
   });
-  const login = useStoreActions(actions => actions.user.login)
-  const isAuthenticated = useStoreState(state => state.user.isAuthenticated)
-  const loading = useStoreState(state => state.user.loading)
-  const { username, password } = formData
+  const login = useStoreActions(actions => actions.user.login);
+  const isAuthenticated = useStoreState(state => state.user.isAuthenticated);
+  const loading = useStoreState(state => state.user.loading);
 
-  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value})
+  const { username, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   const onSubmit = async e => {
-    e.preventDefault()
-    const userLogin = { username, password}
+    e.preventDefault();
+    const userLogin = { username, password }
 
-    login(userLogin)
-    setFormData({username: '', password: ''})
+    login(userLogin);
+    setFormData({ username: '', password: '' })
   };
-
   return (
-    loading ? <h1>Loading...</h1> : (isAuthenticated ? <h1>Go to home page here</h1> :
-    <Container>
-      <h1 className="login">Login</h1>
-      <br/>
-
-      <Form onSubmit={(e) => onSubmit(e)}>
-        <Form.Group controlId="formBasicUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control autofocus name="username" required type="text" placeholder="Enter Username" onChange={(e) => onChange(e)} />
-        </Form.Group>
-
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control name="password" required type="password" placeholder="Enter Password" minLength="6" onChange={(e) => onChange(e)}/>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Log In
-        </Button>
-      </Form>
-    </Container>
+    loading ? <h1>Loading...</h1> : (isAuthenticated ? <Home /> :
+      <MDBContainer className="adminLoginContainer">
+        <MDBCard className="adminLoginCard">
+          <MDBRow className="adminLogin">
+            <MDBCol md="6">
+              <Form onSubmit={(e) => onSubmit(e)}>
+                <p className="h5 text-center mb-4"> Sign in</p>
+                <div className="grey-text">
+                  <MDBInput label="Username" icon="user" group type="text" validate error="wrong"
+                    success="right" name="username" onChange={(e) => onChange(e)} required />
+                  <MDBInput label="Type your password" icon="lock" group type="password" validate name="password" onChange={(e) => onChange(e)} required />
+                </div>
+                <div className="text-center">
+                  <MDBBtn type="submit">Login</MDBBtn>
+                </div>
+              </Form>
+            </MDBCol>
+          </MDBRow>
+        </MDBCard>
+      </MDBContainer>
     )
-  );
+  )
+}
 
-};
-
-export default Login
+export default Login;
