a33eccf0949789dbaf380b74a09bf4fd5c3ec593
Vyas Ramankulangara <vyas01890@outlook.com>
2020-05-05 19:45:31 -0500

    Update index.test.js

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -31,7 +31,7 @@ const dropAllCollections = async () => {
   }
 };
 beforeAll(async () => {
-  const url = `mongodb://localhost/${TEST_DB}`;
+  const url = `mongodb://${HOST}/${TEST_DB}`;
   await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
   await agent
     .post('/api/user/register')
